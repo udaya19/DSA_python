c022dce0297164eb6f498cac68df2677f77ee513
Gonuguntla Udaya Kiran <udaya.kirangonuguntla.20cse@bmu.edu.in>
2022-05-26 12:09:09 +0530

    Circular Linked list

--- linked-list/circular_linkedlist.py
+++ linked-list/circular_linkedlist.py
@@ -1,5 +1,8 @@
 #circular_linkedlist.py
 
+from tkinter import N
+
+
 class Node:
     def __init__(self, data):
         self.data = data 
@@ -29,10 +32,49 @@ class CircularLinkedList:
                 print(temp.data, end=' ')
                 temp = temp.next
             print(temp.data)
-
+    
+    def insert_beg(self,data):
+        nb = Node(data)
+        nb.next = self.head
+        temp = self.head
+        while temp.next != self.head:
+            temp = temp.next
+        temp.next = nb
+        self.head = nb
+    def insert_end(self,data):
+        ne = Node(data)
+        temp = self.head
+        while temp.next != self.head:
+            temp = temp.next
+        temp.next = ne
+        ne.next = self.head
+    # def delete_beg(self):
+    #     temp = self.head
+    #     # temp1 = self.head
+    #     a = self.head.next
+    #     self.head = a
+    #     temp.next = None
+    #     while temp.next != self.head:
+    #         temp = temp.next
+    #     temp.next = self.head
+        # temp1.next = None
+    def deletion_end(self):
+        a = self.head.next
+        temp = self.head
+        while a.next != self.head:
+            a = a.next
+            temp = temp.next
+        temp.next = self.head
+        a.next = None
 cll = CircularLinkedList()
 cll.addList(10)
 cll.addList(20)
 cll.addList(30)
 cll.addList(40)
+cll.traversal()
+cll.insert_beg(0)
+cll.traversal()
+cll.insert_end(50)
+cll.traversal()
+cll.deletion_end()
 cll.traversal()
